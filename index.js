a3163153211056fd42a96ad65281290fe5fc62fa
Benjamin Lupton <b@lupton.cc>
2015-09-05 14:49:54 +0800

    fix typos in comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = 86400000 /* =
 	1000 *  // millisconds in a minute
-	60 *    // seconds in an minute
-	60 *    // minutes in a n hour
+	60 *    // seconds in a minute
+	60 *    // minutes in an hour
 	24      // hours in a day
 */
